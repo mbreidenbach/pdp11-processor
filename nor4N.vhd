9ccc7dd963e362f001b7d1dfb333c2669072c897
mbreidenbach <matt12900@live.com>
2021-08-26 21:39:50 -0400

    nor4: fixed label; nor4N: creation of new 4-way N bit nor gate

--- nor4N.vhd
+++ nor4N.vhd
@@ -1,27 +1,29 @@
 ------------------------------------------------------
 -- Author        : Matthew Breidenbach
--- Design Name   : norN
--- Module Name   : norN
+-- Design Name   : nor4N
+-- Module Name   : nor4N
 -- Architecture  : dataflow
 -- Project Name  : pdp11_processor
 -- Target Devices: Basys3
 --
--- Description: N-bit  bitwise  NOR  unit
+-- Description: 4-way N-bit  bitwise  NOR  unit
 ------------------------------------------------------
 
 library  IEEE;
 use  IEEE.STD_LOGIC_1164.ALL;
 
-entity norN is
+entity nor4N is
     GENERIC (N : INTEGER  := 16); --bit  width
     PORT(
         A : IN  std_logic_vector(N-1  downto  0);
 		B : IN  std_logic_vector(N-1  downto  0);
+		C : IN  std_logic_vector(N-1 downto 0);
+		D : IN  std_logic_vector(N-1 downto 0);
         Y : OUT  std_logic_vector(N-1  downto  0)
     );
-end norN;
+end nor4N;
 
-architecture  dataflow  of norN is
+architecture  dataflow  of nor4N is
 begin
-    Y <= A nor B;
+    Y <= A nor B nor C nor D;
 end  dataflow;
